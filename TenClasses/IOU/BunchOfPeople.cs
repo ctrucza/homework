4d58422e773ab698c578f89ea2672fd38353b5ea
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2015-02-09 14:18:57 +0200

    Add PickPerson()

--- TenClasses/IOU/BunchOfPeople.cs
+++ TenClasses/IOU/BunchOfPeople.cs
@@ -1,20 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IOU
 {
     public class BunchOfPeople
     {
-        private readonly IEnumerable<Person> people;
+        private readonly Person[] people;
 
         public BunchOfPeople(IEnumerable<Person> people)
         {
-            this.people = people;
+            this.people = people.ToArray();
         }
 
         public static BunchOfPeople FindByName(string name)
         {
             IPeopleFacade storage = ServiceLocator.PeopleFacade;
             return storage.FindByName(name);
+        }
+
+        /// <summary>
+        /// High cohesion, but... annoying coupling?
+        /// </summary>
+        public Person PickPerson()
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.Write(i + ".\t");
+                Console.WriteLine(people[i]);
+            }
+
+            int index = Convert.ToInt32(Console.ReadLine());
+            return people[index];
         }
     }
 }
--- TenClasses/IOU/Program.cs
+++ TenClasses/IOU/Program.cs
@@ -10,14 +10,11 @@ namespace IOU
             me.Greet();
 
             string name = WhoMadeYourDay();
-            BunchOfPeople humans = BunchOfPeople.FindByName(name);
-            //Person human = ChooseWisely(humans.ToArray());
-            //human.WriteToStdout();
-
-            //TODO: WriteToConsole()? PickFromConsole()? Write comment. high cohesion, buy annoying coupling? or not
-            //TODO: BunchOfHumans.FindByName() // PickFromStd;
-
-            Console.WriteLine("Test");
+            BunchOfPeople people = BunchOfPeople.FindByName(name);
+            Person person = people.PickPerson();
+           
+            Console.WriteLine(person);
+            Console.ReadLine();
         }
 
         static string WhoMadeYourDay()
@@ -25,18 +22,6 @@ namespace IOU
             Console.WriteLine("Who made your day?");
             string name = Console.ReadLine();
             return name;
-        }
-
-        static Person ChooseWisely(Person[] humans)
-        {
-            for (int i = 0; i < humans.Length; i++)
-            {
-                Console.Write(i + ".\t");
-                //humans[i].WriteToStdout();
-            }
-
-            int no = Convert.ToInt32(Console.ReadLine());
-            return humans [no];
         }
     }
 }
